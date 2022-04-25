45a2223f05a60b95e710a0a4a639fd3b669b2fc8
avms <andrevmuzy@gmail.com>
2022-04-25 17:31:57 -0300

    volume e peso maximo

--- truck_toco.py
+++ truck_toco.py
@@ -1,2 +1,4 @@
 volume_toco = (14*2.6*4.4)
 carga_toco = 6000
+trucktoco = (volume_toco > 160.16), (carga_toco < 6000)
+print("A capacidade maxima do Truck toco e de:", carga_toco, ", com um volume de:", volume_toco)
