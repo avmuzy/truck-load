2068d26f67253255558667eb9deb5f36534f16fe
avms <andrevmuzy@gmail.com>
2022-04-26 16:08:40 -0300

    volume e peso maximo

--- truck_toco.py
+++ truck_toco.py
@@ -2,3 +2,7 @@ volume_toco = (14*2.6*4.4)
 carga_toco = 6000
 trucktoco = (volume_toco > 160.16), (carga_toco < 6000)
 print("A capacidade maxima do Truck toco e de:", carga_toco, ", com um volume de:", volume_toco)
+
+volume_pacote = float(input("Entre com o volume do pacote em metros:"))
+peso_pacote = float(input("Entre como peso do pacote em kg:"))
+pacote = (volume_pacote, peso_pacote)
