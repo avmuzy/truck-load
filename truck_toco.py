2dcd140f7797d20ae529df42998bb9ae3af091f8
avms <andrevmuzy@gmail.com>
2022-05-01 19:43:52 -0300

    setting truck vuc

--- truck_toco.py
+++ truck_toco.py
@@ -1,6 +1,7 @@
 volume_toco = (14*2.6*4.4)
 carga_toco = 6000
-trucktoco = (volume_toco < 160.16), (carga_toco < 6000)
+trucktoco = (volume_toco <
+             -160.16), (carga_toco < 6000)
 print("A capacidade maxima do Truck toco e de:", carga_toco, ", com um volume de:", volume_toco)
 
 volume_pacote = float(input("Entre com o volume do pacote em metros:"))
