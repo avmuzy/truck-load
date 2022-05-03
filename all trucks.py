82e0bb17067b45ff251c70a9eb83957779910236
avms <andrevmuzy@gmail.com>
2022-05-03 12:14:07 -0300

    volume da carga

--- all trucks.py
+++ all trucks.py
@@ -1,5 +1,5 @@
 peso = float(input('Entre com o peso da carga:'))
-
+volume = float(input('Entre com o volume da carga:'))
 volume_vuc = (7.2*2.2*3.5)
 carga_vuc = 3000
 truck_vuc = (volume_vuc < 55.44), (carga_vuc < 3000)
