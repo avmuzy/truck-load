0421983e7ae325aba8f2e358c33def0031a6e418
avms <andrevmuzy@gmail.com>
2022-05-10 14:36:29 -0300

    adding counter

--- truck_toco.py
+++ truck_toco.py
@@ -5,9 +5,10 @@ print('-' *80)
 print("A capacidade maxima do Truck toco e de:", carga_toco, ", com um volume de:", volume_toco)
 print('-' *80)
 
-volume_pacote = 0
-peso_pacote = 0
+volume_pacote = peso_pacote = cont = somapeso = somavolume =0
+
 r = 'S'
+
 while r == 'S' and peso_pacote < carga_toco and volume_pacote < volume_toco:
     print('\033[37m=' * 40, '\033[m')
     volume_pacote = float(input("Entre com o volume do pacote em metros:"))
