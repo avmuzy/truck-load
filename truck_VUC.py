8ad93ecfbbc54aeed309e6fb880ab32c803ce353
avms <andrevmuzy@gmail.com>
2022-05-11 19:15:21 -0300

    truck VUC

--- truck_VUC.py
+++ truck_VUC.py
@@ -5,3 +5,7 @@ truck_vuc = (volume_vuc < 55.44), (carga_vuc < 3000)
 print('-' *80)
 print("A capacidade maxima do Truck VUC e de:{:.2f}Kg , com um volume de:{:.2f}m2".format(carga_vuc, volume_vuc))
 print('-' *80)
+
+volume_pacote = peso_pacote = somapeso = somavolume = cont = 0
+
+r = 'S'
