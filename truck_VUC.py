9f3ec99beae8388adc9c17827145b72f1debe649
avms <andrevmuzy@gmail.com>
2022-05-11 19:15:00 -0300

    truck VUC

---
+++ truck_VUC.py
@@ -0,0 +1,7 @@
+volume_vuc = (7.2*2.2*3.5)
+carga_vuc = 3000
+truck_vuc = (volume_vuc < 55.44), (carga_vuc < 3000)
+
+print('-' *80)
+print("A capacidade maxima do Truck VUC e de:{:.2f}Kg , com um volume de:{:.2f}m2".format(carga_vuc, volume_vuc))
+print('-' *80)
