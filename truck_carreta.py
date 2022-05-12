a02bbc90af41726fa31e46ecad426bf290263c49
avms <andrevmuzy@gmail.com>
2022-05-12 18:37:07 -0300

    conditions

--- truck_carreta.py
+++ truck_carreta.py
@@ -12,3 +12,13 @@ while r == 'S' and peso_pacote < carga_carreta and volume_pacote < volume_carret
     print('\033[37m=' * 40, '\033[m')
     volume_pacote = float(input("Entre com o volume do pacote em metros:"))
     somavolume += volume_pacote
+    cont += 1
+    peso_pacote = float(input("Entre como peso do pacote em kg:"))
+    somapeso += peso_pacote
+    if peso_pacote < carga_carreta:
+        r = str(input('Deseja inserir outro pacote? [S/N]')).strip().upper()
+        if somavolume > volume_carreta:
+            print('\033[31m Limite de volume excedido em {}m2!\033[m'.format(somavolume - volume_carreta))
+        if somapeso > carga_carreta:
+            print('\033[31m Limite de carga excedido em {}Kg\033[m'.format(somapeso - carga_carreta))
+    print('Foram inseridos {} pacotes com um total de {}Kg em {}m2.'.format(cont, somapeso, somavolume))
