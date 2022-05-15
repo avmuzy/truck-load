2a291e21b156cc66d677bd7a9fb04e578c8c22fa
avms <andrevmuzy@gmail.com>
2022-05-15 19:36:55 -0300

    adjusting package count

--- truck_carreta.py
+++ truck_carreta.py
@@ -4,8 +4,8 @@ carreta = (volume_carreta < 207.63), (carga_carreta < 33000)
 print('-' *80)
 print("A capacidade maxima da carreta e de:", carga_carreta, ", com um volume de:", volume_carreta)
 print('-' *80)
-volume_pacote = peso_pacote = cont = somapeso = somavolume =0
-
+volume_pacote = peso_pacote  = somapeso = somavolume =0
+cont = 1
 r = 'S'
 
 while r == 'S' and peso_pacote < carga_carreta and volume_pacote < volume_carreta:
