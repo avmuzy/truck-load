598ef693aad417d6fb221c80b60be69d85ea0d56
avms <andrevmuzy@gmail.com>
2022-05-31 19:08:39 -0300

    f strings

--- truck_toco.py
+++ truck_toco.py
@@ -1,9 +1,9 @@
 volume_toco = (14*2.6*4.4)
 carga_toco = 6000
 trucktoco = (volume_toco == 160.16), (carga_toco == 6000)
-print('-' *80)
+print('-' * 80)
 print("A capacidade maxima do Truck toco e de:", carga_toco, ", com um volume de:", volume_toco)
-print('-' *80)
+print('-' * 80)
 
 volume_pacote = peso_pacote = somapeso = somavolume =0
 cont = 1
