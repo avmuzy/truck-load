d2439569319c910b86893c6cf8100f83a796a361
avms <andrevmuzy@gmail.com>
2022-06-01 19:39:12 -0300

    f strings

--- truck_carreta.py
+++ truck_carreta.py
@@ -1,9 +1,9 @@
 volume_carreta =(18.15*2.6*4.4)
 carga_carreta = 33000
 carreta = (volume_carreta < 207.63), (carga_carreta < 33000)
-print('-' *80)
+print('-' * 80)
 print("A capacidade maxima da carreta e de:", carga_carreta, ", com um volume de:", volume_carreta)
-print('-' *80)
+print('-' * 80)
 volume_pacote = peso_pacote = somapeso = somavolume =0
 cont = 1
 r = 'S'
